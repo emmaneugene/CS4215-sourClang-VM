6cacb916d59eb076e2df2b73fd881e783bf86092
Jie Feng <lowjiefeng1998@gmail.com>
2023-03-15 12:35:19 +0800

    Fix test case issue with \n char
    The template literal in JS also treats \ specially.

--- src/parser/__tests__/sampleProgram.ts
+++ src/parser/__tests__/sampleProgram.ts
@@ -76,9 +76,9 @@ int main() {
   int num2;
   printf("Enter two numbers: ");
   scanf("%d %d", &num1, &num2);
-  printf("Before swap: num1 = %d, num2 = %d", num1, num2);
+  printf("Before swap: num1 = %d, num2 = %d\\n", num1, num2);
   swap(&num1, &num2);
-  printf("After swap: num1 = %d, num2 = %d", num1, num2);
+  printf("After swap: num1 = %d, num2 = %d\\n", num1, num2);
   return 0;
 }
 void swap(int *a, int *b) {
