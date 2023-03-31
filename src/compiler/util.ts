c649df0417f74b9eb8f1ad704fc7161e3ae632d6
Jie Feng <lowjiefeng1998@gmail.com>
2023-03-31 21:45:16 +0800

    Fix update expression

---
+++ src/compiler/util.ts
@@ -0,0 +1,23 @@
+import { TypeList } from 'estree'
+
+import { WORD_SIZE } from './../constants'
+
+/**
+ * Given a certain variable's type, determines how much to update
+ * a variable by when there is an increment/decrement operator on
+ * it.
+ *
+ * E.g. given x++:
+ *
+ * if x is a pointer, increase x's value by 8.
+ * if x is an int/float, increase x's value by 1
+ */
+export function getUpdateSize(typeList: TypeList): number {
+  const actualType = typeList[typeList.length - 1]
+
+  if (actualType === '*') {
+    return WORD_SIZE
+  }
+
+  return 1
+}
